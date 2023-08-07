70149f15d96c342879d39480afa03a626aff2dff
kazuyukiy <tsuzuki.kazuyuki@gmail.com>
2023-08-08 06:41:01 +0900

    introduced src/wc.rs

---
+++ src/wc.rs
@@ -0,0 +1,9 @@
+use std::net::TcpStream;
+
+pub fn response(stream: &mut TcpStream) -> Vec<u8> {
+    //     // request from stream
+    //     // let mut handler = Handler::new(request);
+    //     // handler.response()
+
+    vec![]
+}
