55a3c5ff7f44cd0fb0e3113c0d425cfdd691dadb
kazuyukiy <tsuzuki.kazuyuki@gmail.com>
2023-08-07 15:40:05 +0900

    worker

---
+++ src/lib.rs
@@ -0,0 +1,47 @@
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
+
+// mod thread_pool;
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+struct Job;
+
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(0 < size);
+        let (sender, reciever) = mpsc::channel();
+        let reciever = Arc::new(Mutex::new(reciever));
+        let mut workers = Vec::with_capacity(size);
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&reciever)));
+        }
+        ThreadPool { workers, sender }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    // fn new(id: usize, reciever: mpsc::Receiver<Job>) -> Worker {
+    fn new(id: usize, reciever: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(|| {
+            reciever;
+        });
+        Worker { id, thread }
+    }
+}
