3711e554c5a868961979c9a89685b5c040a89c2a
kazuyukiy <tsuzuki.kazuyuki@gmail.com>
2023-08-15 13:09:53 +0900

    req_info

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,12 @@ use std::convert::Infallible;
 // fn handle handles requests and return responses.
 //
 pub async fn handle(req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    match header(&req, "host") {
-        Ok(v) => println!("host: {}", v),
-        Err(_) => (),
-    }
+    // ~/projects/wc/wc_html/src/page/mod.rs
+    // pub struct Page page.source
+
+    monitor_req_info(&req);
 
     if req.method() == hyper::Method::GET {
-        // println!("method: GET");
         return handle_get();
     }
 
@@ -30,14 +29,25 @@ fn handle_post() -> Result<Response<Body>, Infallible> {
     Ok(Response::new("handle_post".into()))
 }
 
-fn header<'a>(req: &'a Request<Body>, key: &str) -> Result<&'a str, ()> {
-    let headers = req.headers();
-    match headers.get(key) {
-        Some(hv) => match hv.to_str() {
-            Ok(v) => Ok(v),
-            Err(_) => Err(()),
-        },
-        None => Err(()),
+fn monitor_req_info(req: &Request<Body>) {
+    // path
+    println!("== req infor");
+    println!("path: {}", req.uri().path());
+    // path, query
+
+    // headers
+    let header_key = "host";
+    if let Some(ov) = req.headers().get(header_key) {
+        if let Ok(v) = ov.to_str() {
+            println!("{}: {}", header_key, v);
+        }
+    }
+
+    if req.method() == hyper::Method::GET {
+        println!("method: GET");
+    }
+    if req.method() == hyper::Method::POST {
+        println!("method: POST");
     }
 }
 
