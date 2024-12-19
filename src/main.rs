1e926d4036decaee0602bec0d5b531046aa0d441
kazuyukiy <tsuzuki.kazuyuki@gmail.com>
2024-12-19 18:09:17 +0900

    8080

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@
 fn main() {
     // tracing_subscriber::fmt::init();
 
-    // let addr = "127.0.0.1:8080";
-    let addr = "127.0.0.1:3000";
+    let addr = "127.0.0.1:8080";
+    // let addr = "127.0.0.1:3000";
     let page_root_path = "./pages";
 
     let capa = 4;
